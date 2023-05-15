f5490328074d84b5d9fbe8e1ed6543eb5b9cf331
Naitik Raj <user@gmail.com>
2023-05-15 17:31:32 +0530

    changes images and responsive for all devices

--- js/script.js
+++ js/script.js
@@ -7,6 +7,20 @@ menuIcon.onclick = () => {
 };
 
 
+/*========popup window=======*/
+window.addEventListener("load", function(){
+    this.setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },1000
+    )
+});
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
+
+
 /*========scroll sections active link=======*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
